10808498b0c57672cec74c4ef697590ea45b1f4b
Leo Gavin <71537398+DFanso@users.noreply.github.com>
2024-12-30 14:43:52 +0530

    add admin endpoint to auth controller: implement user type validation and error handling for unauthorized access

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -13,7 +13,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from 'src/users/users.service';
 import { SignupDto } from './dto/signup.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-import { AppClsStore } from 'src/Types/users.types';
+import { AppClsStore, UserType } from 'src/Types/users.types';
 import { ClsService } from 'nestjs-cls';
 
 @Controller({ path: 'auth', version: '1' })
@@ -76,6 +76,33 @@ export class AuthController {
     const user = await this.usersService.findOne({ _id: context.user.id });
     if (!user) {
       throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    return {
+      id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      type: user.userType,
+    };
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  @Get('admin')
+  async admin() {
+    const context = this.clsService.get<AppClsStore>();
+    if (!context || !context.user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    console.log(context.user);
+
+    const user = await this.usersService.findOne({ _id: context.user.id });
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    if (user.userType !== UserType.ADMIN) {
+      console.log('User is not admin');
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
     }
     return {
       id: user._id,
