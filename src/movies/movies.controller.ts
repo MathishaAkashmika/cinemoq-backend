73347aca79ef981acf6dc7714f4200b7c1b42b40
Leo Gavin <71537398+DFanso@users.noreply.github.com>
2025-01-05 11:19:38 +0530

    Implement pagination, filtering, and sorting for movie retrieval in Movies module

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -7,8 +7,17 @@ import {
   Param,
   Delete,
   UseGuards,
+  DefaultValuePipe,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Query,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
@@ -26,9 +35,67 @@ export class MovieController {
   }
 
   @Get()
-  @ApiOperation({ summary: 'Get all movies' })
-  findAll() {
-    return this.moviesService.findAll();
+  @ApiOperation({ summary: 'Get paginated list of movies' })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    description: 'Page number',
+    example: 1,
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'Items per page',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'pagination',
+    required: false,
+    description: 'Enable pagination',
+    example: true,
+  })
+  @ApiQuery({
+    name: 'search',
+    required: false,
+    description: 'Search term',
+  })
+  @ApiQuery({
+    name: 'sortBy',
+    required: false,
+    description: 'Sort by field',
+    example: 'createdAt:desc',
+  })
+  @ApiQuery({
+    name: 'genre',
+    required: false,
+    description: 'Filter by genre',
+  })
+  @ApiQuery({
+    name: 'startDate',
+    required: false,
+    description: 'Start date',
+  })
+  @ApiQuery({
+    name: 'endDate',
+    required: false,
+    description: 'End date',
+  })
+  async getPaginatedList(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('pagination', new DefaultValuePipe(true), ParseBoolPipe)
+    pagination: boolean = true,
+    @Query('sortBy', new DefaultValuePipe('createdAt:desc')) sortBy: string,
+    @Query('search', new DefaultValuePipe(undefined))
+    search: string | undefined,
+    @Query('genre') genre?: string,
+    @Query('startDate') startDate?: Date,
+    @Query('endDate') endDate?: Date,
+  ) {
+    return this.moviesService.getPaginatedList({
+      filter: { search, genre, startDate, endDate },
+      options: { page, limit, pagination, sortBy },
+    });
   }
 
   @Get(':id')
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, PaginateModel, PaginateResult } from 'mongoose';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie, MovieDocument } from './entities/movie.entity';
@@ -8,7 +8,8 @@ import { Movie, MovieDocument } from './entities/movie.entity';
 @Injectable()
 export class MoviesService {
   constructor(
-    @InjectModel(Movie.name) private movieModel: Model<MovieDocument>,
+    @InjectModel(Movie.name)
+    private movieModel: Model<MovieDocument> & PaginateModel<MovieDocument>,
   ) {}
 
   async create(createMovieDto: CreateMovieDto): Promise<MovieDocument> {
@@ -16,8 +17,68 @@ export class MoviesService {
     return createdMovie.save();
   }
 
-  async findAll(): Promise<MovieDocument[]> {
-    return this.movieModel.find().exec();
+  async getPaginatedList({
+    filter,
+    options,
+  }: {
+    filter: { [key: string]: any };
+    options: { [key: string]: any };
+  }): Promise<PaginateResult<Movie>> {
+    const queryFilter: any = {};
+
+    // Handle search
+    if (filter.search && filter.search.trim() !== '') {
+      queryFilter.$or = [
+        { name: { $regex: filter.search, $options: 'i' } },
+        { description: { $regex: filter.search, $options: 'i' } },
+        { genre: { $regex: filter.search, $options: 'i' } },
+      ];
+    }
+    delete filter.search;
+
+    // Handle genre filter
+    if (filter.genre) {
+      queryFilter.genre = filter.genre;
+    }
+
+    // Handle date range filter
+    const isValidDate = (date: any) => {
+      if (!date) return false;
+      const parsedDate = new Date(date);
+      return parsedDate instanceof Date && !isNaN(parsedDate.getTime());
+    };
+
+    const startDate = isValidDate(filter.startDate)
+      ? new Date(filter.startDate)
+      : null;
+    const endDate = isValidDate(filter.endDate)
+      ? new Date(filter.endDate)
+      : null;
+
+    if (startDate || endDate) {
+      queryFilter.releaseDate = {};
+
+      if (startDate) {
+        queryFilter.releaseDate.$gte = startDate;
+      }
+
+      if (endDate) {
+        queryFilter.releaseDate.$lte = endDate;
+      }
+    }
+
+    // Handle sorting
+    if (options.sortBy) {
+      options.sort = this.createSortObject(options.sortBy);
+    }
+    delete options.sortBy;
+
+    return this.movieModel.paginate(queryFilter, options);
+  }
+
+  private createSortObject(sortBy: string): Record<string, 1 | -1> {
+    const [field, order] = sortBy.split(':');
+    return { [field]: order === 'desc' ? -1 : 1 };
   }
 
   async findOne(query: any): Promise<MovieDocument | null> {
